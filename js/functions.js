5683615f096585efb4949b2f20abeaba1a97731f
Vanessa Garcia <me@vanessa-lyn.com>
2016-04-05 14:47:17 -0700

    the rest of the animations.

--- js/functions.js
+++ js/functions.js
@@ -8,13 +8,23 @@
     var loadingSpeed = 1; //variable*100 milliseconds
     
     var printMin = 0; //amount of minutes printing will take
-    var printSec = 5; //amount of seconds (minus the min) printing will take
+    var printSec = 1; //amount of seconds (minus the min) printing will take
     
-    
+    var scanMin = 0;
+    var scanSec = 2;
+
+    var verifyMin = 0;
+    var verifySec = 2;
+
+    var sterilizeMin = 0;
+    var sterilizeSec = 2;
+
 
     //grabs amount of milliseconds total from the printing minutes and seconds vars provided. Do Not Adjust This Var 
     var printingMill = ((printMin * 60000) + (printSec * 1000)) / 100;
-
+    var scanningMill = ((scanMin * 60000) + (scanSec * 1000)) / 100;
+    var verifyMill = ((verifyMin * 60000) + (verifySec * 1000)) / 100;
+    var sterilizeMill = ((sterilizeMin * 60000) + (sterilizeSec * 1000)) / 100;
         
 
 
@@ -49,6 +59,25 @@
            setTimeout( printprogress, printingMill );
         }
     }
+
+    function scanprogress() {
+        console.log( 'scanning', scanningMill );
+        var val = $( "#scanningprogress" ).progressbar( "value" ) || 0;
+
+        $( "#scanningprogress" ).progressbar( "value", val + 1 );
+        if ( val < 99 ) {
+           setTimeout( scanprogress, scanningMill );
+        }
+    }
+    function verifyprogress() {
+        console.log( 'scanning', scanningMill );
+        var val = $( "#verifyingprogress" ).progressbar( "value" ) || 0;
+
+        $( "#verifyingprogress" ).progressbar( "value", val + 1 );
+        if ( val < 99 ) {
+           setTimeout( verifyprogress, verifyMill );
+        }
+    }
     function countdown( elementName, minutes, seconds ) {
         var element, endTime, hours, mins, msLeft, time;
 
@@ -59,8 +88,15 @@
         function updateTimer()
         {
             msLeft = endTime - (+new Date);
-            if ( msLeft < 1000 ) {
+            if (( msLeft < 1000 )) {
                 //element.innerHTML = "countdown's over!";
+                if(elementName === "countdownSterilize") {
+                    $(".steps-list .sterilize").removeClass('active').addClass('done');
+                    $(".steps-list .ready").addClass('done');
+
+                    $(".print-steps .sterilizing").hide();
+                    $(".print-steps .ready").show();
+                }
             } else {
                 time = new Date( msLeft );
                 hours = time.getUTCHours();
@@ -78,14 +114,12 @@
     //STEP 2: Show Compiled 3D View of Skull
     $(".view-module .compile-link").click(function(evt) {
         evt.preventDefault();
-        //$(".view-module-wrapper").hide(); 
         $(".compiled-module-wrapper").show(); 
     });
 
     //STEP 3: Show 3d view of new skull piece
     $(".compiled-module .create").click(function(evt) {
         evt.preventDefault();
-        //$(".compiled-module-wrapper").hide(); 
         $(".create-module-wrapper").show(); 
     });
 
@@ -122,18 +156,28 @@
         $(".print-steps .print-complete").hide();
         $(".print-steps .scanning").show();
 
-        setTimeout(function() {
-            $(".steps-list .scan").removeClass('active').addClass('done');
-            $(".steps-list .verify").addClass('active');
+        $( "#scanningprogress" ).progressbar({
+            value: false,
+            complete: function() {
+                $(".steps-list .scan").removeClass('active').addClass('done');
+                $(".steps-list .verify").addClass('active');
 
-            $(".print-steps .scanning").hide();
-            $(".print-steps .verifing").show();
-        }, placeholderTime);
+                $(".print-steps .scanning").hide();
+                $(".print-steps .verifing").show();
 
-        setTimeout(function() {
-            $(".print-steps .verifing").hide();
-            $(".print-steps .verify-complete").show();
-        }, placeholderTime*2);
+                $( "#verifyingprogress" ).progressbar({
+                    value: false,
+                    complete: function() {
+                        $(".print-steps .verifing").hide();
+                        $(".print-steps .verify-complete").show();
+                    }
+                });
+                countdown( "countdownVerify", verifyMin, verifySec );
+                verifyprogress();
+            }
+        });
+        countdown( "countdownScan", scanMin, scanSec );
+        scanprogress();
     });
 
     $(".print-steps .verify-complete button").click(function(evt) {
@@ -144,13 +188,7 @@
         $(".print-steps .verify-complete").hide();
         $(".print-steps .sterilizing").show();
 
-        setTimeout(function() {
-            $(".steps-list .sterilize").removeClass('active').addClass('done');
-            $(".steps-list .ready").addClass('done');
-
-            $(".print-steps .sterilizing").hide();
-            $(".print-steps .ready").show();
-        }, placeholderTime);
+        countdown( "countdownSterilize", sterilizeMin, sterilizeSec );
     });
 
 
